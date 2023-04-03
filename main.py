cfcf58c51d5c6fce6389977abd7ed5d046de5fe1
Sathish Ramani <rsathishx87@gmail.com>
2023-04-02 23:21:45 -0500

    Telegram bot for pap gumroad

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+chat_id = "-915705267"
+
+# https://us-central1-rsathishx87.cloudfunctions.net/pap_gumroad
+# for token, go to BOTFATHER /mybots > apikey
+
+
+def pap_gumroad(request):
+    referrer = request.form.get('referrer')
+    country = request.form.get('ip_country')
+    currency = request.form.get('currency')
+    product = request.form.get('product_name')
+    fee = int(request.form.get('gumroad_fee'))
+    email = request.form.get('email')
+    price = int(request.form.get('price'))
+    full_name = request.form.get('full_name')
+    message = f"🤑💰 KACHING KACHING 🤑💰\n*Product:* {product}\n*Price (After gumroad fee)* - `{currency.upper()}${(price-fee)/100}`\n*User:* {full_name}\n*User Email:* {email}\n*User Country:* {country}\n*Referral Source:* {referrer}\n"
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}&parse_mode=Markdown&disable_web_page_preview=True"
+
+    print(requests.get(url).json())
+
+    return "OK"
